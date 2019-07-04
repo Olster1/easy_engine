c58da0fb9f12bfde37130ff4061d1a6af3ac2c4a
Oliver <oliver_marsh@live.com.au>
2019-07-04 15:11:09 +0930

    working on API

--- src/defines.h
+++ src/defines.h
@@ -3,3 +3,10 @@
 #define OPENGL_MAJOR 3
 #define OPENGL_MINOR 1
 #define RENDER_HANDNESS -1 //Right hand handess -> z going into the screen. 
+
+#if DEVELOPER_MODE
+#define RESOURCE_PATH_EXTENSION "../res/" 
+#else 
+#define RESOURCE_PATH_EXTENSION "res/"
+#define NDEBUG
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,17 @@
-#if DEVELOPER_MODE
-#define RESOURCE_PATH_EXTENSION "../res/" 
-#else 
-#define RESOURCE_PATH_EXTENSION "res/"
-#define NDEBUG
-#endif
-
 #include <GL/gl3w.h>
-
 
 #include "../SDL2/sdl.h"
 #include "../SDL2/SDL_syswm.h"
 
 #include "defines.h"
 #include "easy_headers.h"
-#include "easy_asset_loader.h"
 
-#if __APPLE__
-#include <unistd.h>
-#endif
+// #if __APPLE__
+// #include <unistd.h>
+// #endif
 
 /*
     Have white texture known to the renderer 
-    Have arenas setup automatically 
-    have all the other stuff like monifotr refresh rate setup automatically 
     have it so you don't have to have an extra buffer
     Make button a more reusable concept
     Use matrixes instead of meters per pixels
@@ -34,43 +23,32 @@ int main(int argc, char *args[]) {
     V2 screenDim = v2(0, 0); //init in create app function
     V2 resolution = v2(0, 0);
     screenDim = resolution;
-    OSAppInfo appInfo = easyOS_createApp("Physics 2D", &screenDim, false);
+    bool fullscreen = false;
+    OSAppInfo appInfo = easyOS_createApp("Physics 2D", &screenDim, fullscreen);
     assert(appInfo.valid);
-    
     if(appInfo.valid) {
-        Arena longTermArena = createArena(Kilobytes(200));
-        assets = (Asset **)pushSize(&longTermArena, 4096*sizeof(Asset *));
-        AppSetupInfo setupInfo = easyOS_setupApp(&resolution, RESOURCE_PATH_EXTENSION, &longTermArena);
         
-        float monitorRefreshRate = (float)setupInfo.refresh_rate;
-        float dt = 1.0f / min(monitorRefreshRate, 60.0f); //use monitor refresh rate 
-        float idealFrameTime = 1.0f / 60.0f;
+        easyOS_setupApp(&appInfo, &resolution, RESOURCE_PATH_EXTENSION);
         
         loadAndAddImagesToAssets("img/");
         
         ////INIT FONTS
-        char *fontName = concat(globalExeBasePath, "/fonts/Khand-Regular.ttf");//Roboto-Regular.ttf");/);
+        char *fontName = concatInArena(globalExeBasePath, "/fonts/Khand-Regular.ttf", &globalPerFrameArena);
         Font mainFont = initFont(fontName, 32);
         ///
         FrameBuffer mainFrameBuffer = createFrameBuffer(resolution.x, resolution.y, 0);
-
-        unsigned int lastTime = SDL_GetTicks();
-        bool scrollBarActive = false;
-
-        bool fullscreen = true;
+        
         bool hasBlackBars = true;
         bool running = true;
         AppKeyStates keyStates = {};
-
 
         globalRenderGroup->whiteTexture = findTextureAsset("white.png");
         
         while(running) {
             easyOS_processKeyStates(&keyStates, resolution, &screenDim, &running, !hasBlackBars);
-            easyOS_beginFrame(resolution);
+            easyOS_beginFrame(resolution, &appInfo);
             
             beginRenderGroupForFrame(globalRenderGroup);
-            
 
             clearBufferAndBind(appInfo.frameBackBufferId, COLOR_BLACK);
             clearBufferAndBind(mainFrameBuffer.bufferId, COLOR_PINK);
@@ -80,13 +58,15 @@ int main(int argc, char *args[]) {
             renderSetViewPort(0, 0, resolution.x, resolution.y);
 
             ///Drawing here
-            
+            outputText(&mainFont, 0.5f*resolution.x, 0.5f*resolution.y, -1, resolution, "hey there", rect2fMinMax(0, 0, 1000, 1000), COLOR_BLACK, 1, true, appInfo.screenRelativeSize);
+            //What is should be...size should be baked into the font. 
+            // outputText(&mainFont, 0.5f*resolution.x, 0.5f*resolution.y, -1, "hey there");
 
             //
                 
             drawRenderGroup(globalRenderGroup);
             
-            easyOS_endFrame(resolution, screenDim, &dt, appInfo.windowHandle, mainFrameBuffer.bufferId, appInfo.frameBackBufferId, appInfo.renderBackBufferId, &lastTime, idealFrameTime, hasBlackBars);
+            easyOS_endFrame(resolution, screenDim, mainFrameBuffer.bufferId, &appInfo, hasBlackBars);
             easyOS_endKeyState(&keyStates);
         }
         easyOS_endProgram(&appInfo);
